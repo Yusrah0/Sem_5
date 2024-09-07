c570de224b12579addba816926e2fadfcb3ab1bb
Yusrah0 <130045060+Yusrah0@users.noreply.github.com>
2024-09-07 18:24:46 +0530

    Update Merge Two Sorted Arrays.cpp

--- yusrah/Week 5/Merge Two Sorted Arrays.cpp
+++ yusrah/Week 5/Merge Two Sorted Arrays.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int n,m,k=0,g,f,arr1[15],arr2[15],res[30];
+	int n,m,k=0,arr1[15],arr2[15],res[30];
 	printf("Enter the size of array 1\n");
 	scanf("%d",&n);
 	printf("Enter the size of array 2\n");
@@ -13,8 +13,7 @@ int main(){
 	for(int i=0;i<n;i++){
 	scanf("%d",arr2[i]);
 }
-g=n;f=m;
-while(g!=0||f!=0){
+while(n>0||m>0){
 if(arr1[n]>arr2[m]){
 	res[k]=arr1[n];
 	n--;
