8de3bb27f9e45afd0cd764936a124b559c8f94ff
Yusrah0 <130045060+Yusrah0@users.noreply.github.com>
2024-09-10 12:50:52 +0530

    Update Count Vowel and Consonant.cpp

--- yusrah/Week 3/Count Vowel and Consonant.cpp
+++ yusrah/Week 3/Count Vowel and Consonant.cpp
@@ -3,12 +3,9 @@
 int main(){
 	char word[20];
 	int n,vow=0,cons=0;
-	printf("Enter the size of string\n");
-	scanf("%d",&n);
 	printf("Enter the string\n");
-	for(int i=0;i<n;i++){
-		scanf("%c",&word[i]);
-	}
+	fgets(word,20,stdin);
+	n=strlen(word);
 	for(int i=0;i<n;i++){
 		if(word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'||word[i]=='A'||word[i]=='E'||word[i]=='I'||word[i]=='O'||word[i]=='U'){
 			vow++;
