7ef3feda17b7f40544dd11e41dd51215a3316da1
Yusrah0 <130045060+Yusrah0@users.noreply.github.com>
2024-10-02 23:08:12 +0530

    Update Print Hello World and distribute work randomly.c

--- OpenMP/Week 3/Print Hello World and distribute work randomly.c
+++ OpenMP/Week 3/Print Hello World and distribute work randomly.c
@@ -19,3 +19,30 @@ int main() {
 
     return 0;
 }
+or 
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int main() {
+    int i;
+
+    // Start the parallel region with 4 threads
+    #pragma omp parallel num_threads(4) private(i)
+    {
+        int thread_id = omp_get_thread_num();
+        unsigned int seed = time(NULL) + thread_id;  // Each thread has its own unique seed
+
+        // Use dynamic scheduling to distribute iterations among threads
+        #pragma omp for schedule(dynamic)
+        for (i = 0; i < 20; i++) {
+            if (rand_r(&seed) % 4 == thread_id) {
+                printf("Thread %d: Hello World! (Iteration %d)\n", thread_id, i);
+            }
+        }
+    }
+
+    return 0;
+}
+
